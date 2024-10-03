c7b6275fffcc0fb8a28d7855bca3a2a33a36361a
Lauda D Raka <laudadraka@gmail.com>
2024-10-03 22:29:55 +0700

    Feature/Added Components

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,12 +4,16 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+<script>
+// import content from "../components/content.vue";
+export default {
+  name: "IndexPage",
+  components: {
+    // content,
+  },
+};
 </script>
