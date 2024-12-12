1950aa90901f24ccd472de11102dd7cc3ce8152c
ulyaslsabila <imtitsalulya@gmail.com>
2024-12-12 12:43:51 +0700

    Add User Detail Page

---
+++ src/components/UserDetailPage/UserDetailContent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="row q-my-sm"
+    style="background-color: #E2EFFF; font-size: 1.2rem; margin-left:30px; margin-right:30px;margin-top:120px;margin-bottom:10px;"
+    >
+    <div class="row q-my-sm" style="background-color: #FFFFFF; width: 100%; margin-left:150px; margin-right:150px;margin-top:15px; margin-bottom:15px;">
+        <div class="col q-my-sm" style="background-color: #FFFFFF; width: 100%; margin-left:15px; margin-right:15px;margin-top:25px; text-align: left;">
+            <div class="value" style="font-size: 2.5rem;"> Hai, Ayman Shafwan! </div>
+            <div class="span"> Email         : ayman40@gmail.com</div>
+            <div class="span"> Phone Number  : +62 812-487-860</div>
+        </div>
+    </div>
+  </div>
+
+</template>
+
+
+<style scoped>
+/* Gaya untuk container rounded rectangle */
+.row.q-my-sm {
+  border-radius: 10px; /* Membuat sudut membulat */
+  overflow: hidden; /* Opsional, untuk memastikan isi container tetap rapi */
+}
+
+/* Gaya untuk label */
+.span {
+  color: #555; /* Warna abu-abu untuk label */
+  font-size: 0.9rem; /* Ukuran font lebih kecil untuk label */
+  font-weight: normal; /* Berat font normal */
+}
+
+/* Gaya untuk value */
+.value {
+  color: #333; /* Warna lebih gelap untuk value */
+  font-size: 1.2rem; /* Ukuran font lebih besar untuk value */
+  font-weight: bold; /* Berat font bold */
+}
+
+</style>
---
+++ src/pages/UserDetailPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page>
+    <UserDetailContent />
+  </q-page>
+</template>
+
+<script>
+import UserDetailContent from "src/components/UserDetailPage/UserDetailContent.vue";
+
+export default {
+  name: "UserDetailPage",
+  components: {
+    UserDetailContent,
+  },
+};
+</script>
+
+<style scoped>
+/* Add styles specific to IndexPage here if needed */
+</style>
