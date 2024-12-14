375fbda2bbb62567ad62bf27d13356992dcc5eff
SalsaHidi <148998075+SalsaHidi@users.noreply.github.com>
2024-12-14 15:13:37 +0700

    add_itinerary(belum rapi)

--- src/components/IndexPage/DestinasiWisata.vue
+++ src/components/IndexPage/DestinasiWisata.vue
@@ -1,81 +1,126 @@
 <template>
   <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
     rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
   />
-  <div class="row row-cols-1 row-cols-md-3 g-4">
-    <div class="col">
-      <div class="card h-100">
-        <img
-          src="public/dufan.jpg"
-          class="card-img-top"
-          alt="Dufan Taman Impian Ancol"
-        />
-        <div class="card-body">
-          <h5 class="card-title">Dufan Taman Impian Ancol</h5>
-          <p class="card-text">
-            “Dufan (Dunia Fantasi) merupakan taman hiburan di Ancol, Jakarta,
-            yang menawarkan wahana seru dengan tema internasional seperti Eropa,
-            Amerika, dan Asia. Cocok untuk segala usia, Dufan menjadi destinasi
-            favorit rekreasi keluarga.”
-          </p>
+
+  <div class="container">
+    <h3>Itinerary Kami</h3>
+    <div class="row g-10">
+      <div class="col-md-3">
+        <div class="card h-100 d-flex flex-column">
+          <img src="public/jakarta.jpg" class="card-img-top" alt="Jakarta" />
+          <div class="card-body">
+            <h5 class="card-title">Jakarta</h5>
+          </div>
         </div>
-        <div class="card-footer">
-          <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card h-100 d-flex flex-column">
+          <img src="public/semarang.jpg" class="card-img-top" alt="Semarang" />
+          <div class="card-body">
+            <h5 class="card-title">Semarang</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card h-100 d-flex flex-column">
+          <img src="public/bogor.jpg" class="card-img-top" alt="Bogor" />
+          <div class="card-body">
+            <h5 class="card-title">Bogor</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card h-100 d-flex flex-column">
+          <img src="public/bandung.jpg" class="card-img-top" alt="Bandung" />
+          <div class="card-body">
+            <h5 class="card-title">Bandung</h5>
+          </div>
         </div>
       </div>
     </div>
-    <div class="col">
-      <div class="card h-100">
-        <img
-          src="public/lembang.jpg"
-          class="card-img-top"
-          alt="Lembang Park Zoo"
-        />
-        <div class="card-body">
-          <h5 class="card-title">Lembang Park Zoo</h5>
-          <p class="card-text">
-            “Lembang Park & Zoo adalah kebun binatang modern di Lembang,
-            Bandung, yang menawarkan koleksi satwa beragam, fasilitas rekreasi,
-            dan area bermain keluarga. Tempat ini juga dilengkapi kafe unik dan
-            wahana edukasi interaktif.”
-          </p>
+  </div>
+
+  <div class="container">
+    <h3>Tujuan Destinasi</h3>
+    <div class="row g-4">
+      <div class="col-md-3">
+        <div class="card h-100 d-flex flex-column">
+          <img
+            src="public/dufan.jpg"
+            class="card-img-top"
+            alt="Dufan Taman Impian Ancol"
+          />
+          <div class="card-body">
+            <h5>Dufan Taman Impian Ancol</h5>
+            <p class="card-text">
+              Dufan (Dunia Fantasi) merupakan taman hiburan di Ancol, Jakarta,
+              yang menawarkan wahana seru dengan tema internasional seperti
+              Eropa, Amerika, dan Asia. Cocok untuk segala usia, Dufan menjadi
+              destinasi favorit rekreasi keluarga.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <div class="card h-100">
-        <img
-          src="public/bogor.jpg"
-          class="card-img-top"
-          alt="Kebun Raya Bogor"
-        />
-        <div class="card-body">
-          <h5 class="card-title">Kebun Raya Bogor</h5>
-          <p class="card-text">
-            “Kebun Raya Bogor adalah taman botani di Bogor, Jawa Barat, yang
-            memiliki koleksi ribuan jenis tanaman dari seluruh dunia. Tempat ini
-            terkenal sebagai pusat penelitian, wisata edukasi, dan rekreasi
-            alam.”
-          </p>
+
+      <div class="col-md-3">
+        <div class="card h-100 d-flex flex-column">
+          <img
+            src="public/lembang.jpg"
+            class="card-img-top"
+            alt="Lembang Park Zoo"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Lembang Park Zoo</h5>
+
+            <p class="card-text">
+              Lembang Park & Zoo adalah kebun binatang modern di Lembang,
+              Bandung, yang menawarkan koleksi satwa beragam, fasilitas
+              rekreasi, dan area bermain keluarga. Tempat ini juga dilengkapi
+              kafe unik dan wahana edukasi interaktif.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <div class="card h-100">
-        <img
-          src="public/lawang_sewu.jpg"
-          class="card-img-top"
-          alt="Museum Lawang Sewu"
-        />
-        <div class="card-body">
-          <h5 class="card-title">Museum Lawang Sewu</h5>
-          <p class="card-text">
-            “Museum Lawang Sewu di Semarang adalah bangunan bersejarah dengan
-            arsitektur kolonial Belanda yang ikonik. Dulunya kantor perusahaan
-            kereta api, kini menjadi destinasi wisata edukasi yang sarat cerita
-            sejarah dan budaya.”
-          </p>
+
+      <div class="col-md-3">
+        <div class="card h-100 d-flex flex-column">
+          <img
+            src="public/kebun_raya.jpg"
+            class="card-img-top"
+            alt="Kebun Raya Bogor"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Kebun Raya Bogor</h5>
+            <p class="card-text">
+              Kebun Raya Bogor adalah taman botani yang memiliki koleksi ribuan
+              jenis tanaman dari seluruh dunia. Tempat ini terkenal sebagai
+              pusat penelitian, wisata edukasi, dan rekreasi alam.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card h-100 d-flex flex-column">
+          <img
+            src="public/lawang_sewu.jpg"
+            class="card-img-top"
+            alt="Museum Lawang Sewu"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Museum Lawang Sewu</h5>
+            <p class="card-text">
+              Museum Lawang Sewu di Semarang adalah bangunan bersejarah dengan
+              arsitektur kolonial Belanda yang ikonik. Dulunya kantor perusahaan
+              kereta api, kini menjadi destinasi wisata edukasi yang sarat
+              cerita sejarah dan budaya.
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -90,5 +135,7 @@ export default {
 </script>
 
 <style scoped>
-/* CSS di sini */
+.card-body {
+  min-height: 200px; /* Atur tinggi minimum sesuai kebutuhan */
+}
 </style>
